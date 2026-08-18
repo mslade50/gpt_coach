4129db647f76795e4f4464437fa5e8045d99a372
mslade50 <107962250+mslade50@users.noreply.github.com>
2026-08-18 15:11:22 -0400

    Load Week 4 Day 2 completion override

--- sunday-preview.js
+++ sunday-preview.js
@@ -24,7 +24,8 @@
     "data/week3-threshold-swap.js",
     "data/week3-w3d3-complete.js",
     "data/week3-w3d4-complete.js",
-    "data/week4-w4d1-results.js"
+    "data/week4-w4d1-results.js",
+    "data/week4-w4d2-complete.js"
   ];
 
   const loadSequentially = (index = 0) => {
